eff98407ad403a9fe4851956b261264fbdbbbed8
Jeroen Kolk <jeroen.kolk@sogeti.com>
2021-06-18 21:14:24 +0200

    videoDetail

--- search-app-udemy/src/components/App.js
+++ search-app-udemy/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "./Searchbar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
@@ -19,11 +20,15 @@ class App extends React.Component {
     this.setState({ selectedVideo: video });
   }
 
+
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-       <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+       
 
       </div>
       
